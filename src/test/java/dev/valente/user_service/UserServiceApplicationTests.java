efb548b2c563ddbc20a92b9ae9739384b2c666e9
Gabriel Valente <112831085+ValentePG@users.noreply.github.com>
2025-01-08 09:25:29 -0300

    aula 76

--- src/test/java/dev/valente/user_service/UserServiceApplicationTests.java
+++ src/test/java/dev/valente/user_service/UserServiceApplicationTests.java
@@ -2,6 +2,7 @@ package dev.valente.user_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 class UserServiceApplicationTests {
