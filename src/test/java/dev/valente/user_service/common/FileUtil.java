26bc31d9d9d5fd0646a9df441de997570a7a67c6
Gabriel Valente <112831085+ValentePG@users.noreply.github.com>
2025-01-16 20:08:33 -0300

    UserControllerRestAssured completo

--- src/test/java/dev/valente/user_service/common/FileUtil.java
+++ src/test/java/dev/valente/user_service/common/FileUtil.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
 import java.nio.file.Files;
 
 @Component
--- src/test/java/dev/valente/user_service/user/controller/UserControllerRestAssuredTest.java
+++ src/test/java/dev/valente/user_service/user/controller/UserControllerRestAssuredTest.java
@@ -3,6 +3,7 @@ package dev.valente.user_service.user.controller;
 import dev.valente.user_service.common.FileUtil;
 import dev.valente.user_service.common.UserDataUtil;
 import dev.valente.user_service.config.IntegrationTestConfig;
+import dev.valente.user_service.domain.User;
 import dev.valente.user_service.user.repository.UserRepositoryJPA;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
@@ -11,18 +12,17 @@ import net.javacrumbs.jsonunit.core.Option;
 import org.assertj.core.api.Assertions;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.*;
-import org.mockito.BDDMockito;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.util.Optional;
+import java.util.stream.Stream;
 
 @ComponentScan(basePackages = {"dev.valente.user_service.user", "dev.valente.user_service.common"})
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -107,7 +107,7 @@ public class UserControllerRestAssuredTest extends IntegrationTestConfig {
     @Order(3)
     @Sql(value = "/sql/init_sql_threeusers.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     @Sql(value = "/sql/drop_users.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
-    void findById_shouldReturnUser_whenSuccessfull() throws Exception {
+    void findById_shouldReturnUser_whenSuccessfull() {
         var pathResponse = "/user/get/get_findbyid_200.json";
         var expectedUserFromFile = fileUtil.readFile(pathResponse);
 
@@ -139,7 +139,7 @@ public class UserControllerRestAssuredTest extends IntegrationTestConfig {
     @Test
     @DisplayName("GET v1/users/{inexistentId} should return NOT FOUND")
     @Order(4)
-    void findById_shouldReturnNotFound_whenFailed() throws Exception {
+    void findById_shouldReturnNotFound_whenFailed() {
 
         var response = fileUtil.readFile("/user/get/get_findbyid-inexistentid_404.json");
 
@@ -163,7 +163,7 @@ public class UserControllerRestAssuredTest extends IntegrationTestConfig {
     @Order(5)
     @Sql(value = "/sql/init_sql_threeusers.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     @Sql(value = "/sql/drop_users.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
-    void findWithParamsWithEmail_shouldReturnUser_whenSuccessfull() throws Exception {
+    void findWithParamsWithEmail_shouldReturnUser_whenSuccessfull() {
         var pathResponse = "/user/get/get_findbyemail_200.json";
         var expectedUserFromFile = fileUtil.readFile(pathResponse);
 
@@ -189,7 +189,7 @@ public class UserControllerRestAssuredTest extends IntegrationTestConfig {
     @Test
     @DisplayName("GET v1/users/find?email=inexistentEmail should return NOT FOUND")
     @Order(6)
-    void findWithParamsWithNewEmail_shouldReturnNotFound_whenFailed() throws Exception {
+    void findWithParamsWithNewEmail_shouldReturnNotFound_whenFailed() {
 
         var response = fileUtil.readFile("/user/get/get_findbyemail-inexistentemail_404.json");
 
@@ -211,7 +211,7 @@ public class UserControllerRestAssuredTest extends IntegrationTestConfig {
     @Order(7)
     @Sql(value = "/sql/init_sql_threeusers.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     @Sql(value = "/sql/drop_users.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
-    void findWithParamsWithFirstName_shouldReturnUser_whenSuccessfull() throws Exception {
+    void findWithParamsWithFirstName_shouldReturnUser_whenSuccessfull() {
         var pathResponse = "/user/get/get_findbyfirstname_200.json";
         var expectedUserFromFile = fileUtil.readFile(pathResponse);
 
@@ -237,7 +237,7 @@ public class UserControllerRestAssuredTest extends IntegrationTestConfig {
     @Test
     @DisplayName("GET v1/users/find?firstName=inexistentFirstName should return NOT FOUND")
     @Order(8)
-    void findWithParamsWithFirstName_shouldReturnNotFound_whenFailed() throws Exception {
+    void findWithParamsWithFirstName_shouldReturnNotFound_whenFailed() {
 
         var response = fileUtil.readFile("/user/get/get_findbyfirstname-inexistfirstname_404.json");
 
@@ -255,4 +255,205 @@ public class UserControllerRestAssuredTest extends IntegrationTestConfig {
 
     }
 
+    @Test
+    @DisplayName("POST v1/users should save and return a user")
+    @Order(9)
+    void save_shouldSaveAndReturnUser_whenSuccessfull() {
+        var pathResponse = "/user/post/post_createduser_201.json";
+        var pathRequest = "/user/post/post_createuser_200.json";
+
+        var request = fileUtil.readFile(pathRequest);
+        var expectedUserCreated = fileUtil.readFile(pathResponse);
+
+        var response = RestAssured.given()
+                .contentType(ContentType.JSON).accept(ContentType.JSON)
+                .body(request)
+                .when()
+                .post(URL)
+                .then()
+                .statusCode(HttpStatus.CREATED.value())
+                .log().all()
+                .extract().response().body().asString();
+
+        JsonAssertions.assertThatJson(response)
+                .whenIgnoringPaths("id")
+                .isEqualTo(expectedUserCreated);
+
+    }
+
+    @ParameterizedTest
+    @MethodSource("postParameterizedTest")
+    @DisplayName("POST v1/users should return BAD REQUEST")
+    @Order(10)
+    void save_shouldReturnBadRequest_whenFailed(String fileName) {
+
+        var request = fileUtil.readFile(fileName);
+        var responseFile = fileUtil.readFile("/user/post/post_createuser-badrequest_400.json");
+
+        var response = RestAssured.given()
+                .contentType(ContentType.JSON).accept(ContentType.JSON)
+                .body(request)
+                .when()
+                .post(URL)
+                .then()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .log().all()
+                .extract().response().body().asString();
+
+
+        JsonAssertions.assertThatJson(response)
+                .whenIgnoringPaths("timestamp")
+                .isEqualTo(responseFile);
+
+
+    }
+
+    private static Stream<Arguments> postParameterizedTest() {
+
+        return Stream.of(
+                Arguments.of("/user/post/post_createuser-empty-values_400.json"),
+                Arguments.of("/user/post/post_createuser-blank-values_400.json")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("putParametrizedTest")
+    @DisplayName("PUT v1/users payload with existentId should replace a user with valid email, firstName and Lastname")
+    @Order(11)
+    @Sql(value = "/sql/init_sql_threeusers.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(value = "/sql/drop_users.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    void replaceWithValidPayload_shouldReplaceUserWithValidPayload_whenSuccessfull(User user) {
+
+        var existentUser = userRepository.findUserByFirstName("Johny");
+        Assertions.assertThat(existentUser).isNotEmpty();
+        user.setId(existentUser.get().getId());
+
+        RestAssured.given()
+                .contentType(ContentType.JSON).accept(ContentType.JSON)
+                .body(user)
+                .when()
+                .put(URL)
+                .then()
+                .statusCode(HttpStatus.NO_CONTENT.value())
+                .log().all();
+    }
+
+    private static Stream<Arguments> putParametrizedTest() {
+        var withNewEmail = User.builder().email("jorjin@gmail.com").build();
+        var withNewFirstName = User.builder().firstName("Joaquin").build();
+        var withNewLastName = User.builder().lastName("Bravo").build();
+
+        return Stream.of(
+                Arguments.of(withNewEmail),
+                Arguments.of(withNewFirstName),
+                Arguments.of(withNewLastName)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("putWithInvalidFields")
+    @DisplayName("PUT v1/users payload should return BAD REQUEST with invalid fields")
+    @Order(12)
+    void replaceWithInvalidPayload_shouldReturnBadRequest_whenFailed(String fileName) {
+        var request = fileUtil.readFile(fileName);
+
+        var responseFile = fileUtil.readFile("/user/put/put_replacebadrequest_400.json");
+
+        var response = RestAssured.given()
+                .contentType(ContentType.JSON).accept(ContentType.JSON)
+                .body(request)
+                .when()
+                .put(URL)
+                .then()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .log().all()
+                .extract().response().body().asString();
+
+        JsonAssertions.assertThatJson(response)
+                .whenIgnoringPaths("timestamp")
+                .isEqualTo(responseFile);
+
+    }
+
+    private static Stream<Arguments> putWithInvalidFields() {
+        var invalidEmail = "/user/put/put_replacewithinvalidemail_400.json";
+        var blankEmail = "/user/put/put_replacewithblank-empty-email_400.json";
+        var blankFirstName = "/user/put/put_replacewithblank-empty-firstname_400.json";
+        var blankLastName = "/user/put/put_replacewithblank-empty-lastname_400.json";
+
+        return Stream.of(
+                Arguments.of(invalidEmail),
+                Arguments.of(blankEmail),
+                Arguments.of(blankFirstName),
+                Arguments.of(blankLastName)
+        );
+    }
+
+    @Test
+    @DisplayName("PUT v1/users payload with inexistentId should return NOT FOUND")
+    @Order(13)
+    void replace_shouldReturnNotFound_whenFailed() {
+        var pathRequest = "/user/put/put_replacewithinexistentid_404.json";
+
+        var pathResponse = "/user/put/put_replacenotfound_404.json";
+
+        var request = fileUtil.readFile(pathRequest);
+
+        var response = fileUtil.readFile(pathResponse);
+
+        RestAssured.given()
+                .contentType(ContentType.JSON).accept(ContentType.JSON)
+                .body(request)
+                .when()
+                .put(URL)
+                .then()
+                .statusCode(HttpStatus.NOT_FOUND.value())
+                .body(Matchers.equalTo(response))
+                .log().all();
+
+    }
+
+    @Test
+    @DisplayName("DELETE v1/users/{existentId} should delete user")
+    @Order(14)
+    @Sql(value = "/sql/init_sql_threeusers.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(value = "/sql/drop_users.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    void deleteById_shouldDeleteUser_whenSuccessfull() {
+
+        var existentUserId = userRepository.findUserByFirstName("Johny").get().getId();
+
+
+        RestAssured.given()
+                .contentType(ContentType.JSON).accept(ContentType.JSON)
+                .when()
+                .pathParam("id", existentUserId)
+                .delete(URL + "/{id}")
+                .then()
+                .statusCode(HttpStatus.NO_CONTENT.value())
+                .log().all();
+
+    }
+
+    @Test
+    @DisplayName("DELETE v1/users/{inexistentId} should return NOT FOUND")
+    @Order(15)
+    void deleteById_shouldReturnNotFound_whenFailed() {
+
+        var pathResponse = "/user/delete/delete_idnotfound_404.json";
+
+        var response = fileUtil.readFile(pathResponse);
+
+        var inexistentId = 50L;
+
+        RestAssured.given()
+                .contentType(ContentType.JSON).accept(ContentType.JSON)
+                .when()
+                .pathParam("id", inexistentId)
+                .delete(URL + "/{id}")
+                .then()
+                .statusCode(HttpStatus.NOT_FOUND.value())
+                .body(Matchers.equalTo(response))
+                .log().all();
+
+    }
 }
